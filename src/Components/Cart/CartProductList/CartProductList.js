fae0ed8e186b5f74b233f25d7193b7207b5e6a48
MarcinSufa <sufa.marcin@gmail.com>
2019-05-22 10:48:22 +0200

    Mobile styling

--- src/Components/Cart/CartProductList/CartProductList.js
+++ src/Components/Cart/CartProductList/CartProductList.js
@@ -9,13 +9,12 @@ const CartProductList = (props) => {
     
     return (
         <div className='ProductInCart' key={props.id}  onClick={props.clicked}> 
-        <div className='cartProductDelete' ><img className='SmallProductImage' src={props.image} alt={props.alt}></img></div>
-            <div className='CartProductInfo'><h4>Brand</h4><p>{props.brand}</p></div>
-            <div className='CartProductInfo'><h4>Model</h4><p>{props.model}</p></div>
-            <div className='CartProductInfo'><h4>Quantity</h4><p>{props.quantity}</p></div>
+        <div className='cartProductWrapper' ><img className='SmallProductImage' src={props.image} alt={props.alt}></img></div>
+            <div className='CartProductInfo'><h4>{props.brand}</h4><p>{props.model}</p></div>
+            <div className='CartProductInfo'><h4>No.</h4><p>{props.quantity}</p></div>
             <div className='CartProductInfo'><h4>Size</h4><p>{props.size}</p> </div>
             <div className='CartProductInfo'><h4>Price</h4><p>{props.price}$</p> </div>
-            <div className='cartProductDelete'><Button btnType={props.btnType} value={props.id} clicked={props.onClick}>Delete</Button></div>
+            <div className={'cartProductDelete ' + props.noBtn}><Button value={props.id} clicked={props.onClick}>Delete</Button></div>
         </div> 
     );
     
--- src/Containers/Account/Account.js
+++ src/Containers/Account/Account.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './Account.css';
+import './Account.modules.css';
 import Spinner from "../../Components/UI/Spinner/Spinner";
 import { connect } from 'react-redux';
 import axios from '../../axios-products';
 import CartProductList from '../../Components/Cart/CartProductList/CartProductList';
-
+import Button from '../../Components/UI/Button/Button';
 
 class Account extends Component {
 
@@ -26,7 +26,7 @@ class Account extends Component {
             console.log(this.state.accountData);
         })
         .catch( error => {
-            this.setState( { error: error, loading: false } );
+            this.setState({ error: error, loading: false } );
         })
     } 
 
@@ -36,7 +36,11 @@ class Account extends Component {
                 {showProducts: !prevState.showProducts}
             )
         });
-        this.setState({ProductId: id})  
+        if (id!== this.state.ProductId) {
+            this.setState({showProducts: true, ProductId: id })
+        } else {
+            this.setState({ProductId: id}) 
+        }
     }
 
 render () {
@@ -50,17 +54,16 @@ render () {
         console.log(Object.entries(this.state.accountData));
         showOrders = (Object.entries(this.state.accountData).map((order) => {
             return (
-            <div key={order[0]}>
-                    <div className='ProductInCart'  onClick={() => {this.openProductList(order[0])}}> 
-                    <div className='CartProductInfo'><h4>Price: </h4><p>{order[1].Price} $</p></div>
-                    <div className='CartProductInfo'><h4>Email</h4> <p>{order[1].CustomerData.email.value}</p> </div>
-                    <div className='CartProductInfo'><h4>Name:</h4> <p>{order[1].CustomerData.name.value}</p></div>
-                    <div className='CartProductInfo'><h4>Street:</h4> <p>{order[1].CustomerData.street.value}</p></div>
-                    <div className='CartProductInfo'><h4>Zip Code:</h4> <p>{order[1].CustomerData.zipCode.value}</p></div>
-                    <div className='CartProductInfo'><h4>No:</h4> <p>{order[1].OrderDetails.length}</p></div>
-                    <div className='CartProductInfo'><h4>Date:</h4> <p>{order[1].OrderDate}</p></div> 
+            <div className='Account' key={order[0]}>
+                    <div className='OrderWrapper'  > 
+                    <div className='OrderProductInfo'><h4>Date</h4> <p>{order[1].OrderDate}</p></div> 
+                    <div className='OrderProductInfo'><h4>Email</h4> <p>{order[1].CustomerData.email.value}</p> </div>
+                    <div className='OrderProductInfo'><h4>Name</h4> <p>{order[1].CustomerData.name.value}</p></div>
+                    <div className='OrderProductInfo'><h4>Street</h4> <p>{order[1].CustomerData.street.value}</p></div>
+                    <div className='OrderProductInfo'><h4>Total </h4><p>{order[1].Price} $</p></div>
+                    <div className='OrderProductBtn'><Button clicked={() => {this.openProductList(order[0])}}>Details</Button></div>
                 </div>
-                { (this.state.showProducts && order[0] === this.state.ProductId? ( Object.entries(order[1].OrderDetails).map((prod) => {
+                {(this.state.showProducts && order[0] === this.state.ProductId? ( Object.entries(order[1].OrderDetails).map((prod) => {
                         return (
                             <CartProductList
                                 key={prod[0]}
@@ -72,7 +75,7 @@ render () {
                                 quantity={prod[1].quantity}
                                 size={prod[1].size}
                                 price={prod[1].price}
-                                btnType='noBtn'
+                                noBtn='noBtn'
                             />
                             );   
                     }) 
@@ -84,7 +87,7 @@ render () {
     }
 
     return (
-        <div>
+        <div className='OrdersLayout'>
             {spinner}
             <h1>Your Orders</h1>
             {showOrders}
