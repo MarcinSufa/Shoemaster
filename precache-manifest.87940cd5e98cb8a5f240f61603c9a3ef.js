1be10e49e33d6bc1f28a2b5de2da163628a65291
MarcinSufa <sufa.marcin@gmail.com>
2019-05-23 15:58:37 +0200

    Updates

---
+++ precache-manifest.87940cd5e98cb8a5f240f61603c9a3ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2861e7cc026fc4f03ab1a47bb36780fa",
+    "url": "/Shoemaster/static/media/warning.2861e7cc.png"
+  },
+  {
+    "revision": "14520b56bf12cd1ab7b5d24439cf45b8",
+    "url": "/Shoemaster/static/media/shopping-cart.14520b56.svg"
+  },
+  {
+    "revision": "1bc447451e3cdb87c3bd6e473c0e7377",
+    "url": "/Shoemaster/static/media/empty.1bc44745.png"
+  },
+  {
+    "revision": "46ea2fc3783fcbd5af69134990c63c16",
+    "url": "/Shoemaster/static/media/done.46ea2fc3.png"
+  },
+  {
+    "revision": "e237f2de7e3b1ae5d174",
+    "url": "/Shoemaster/static/js/runtime~main.ca2e143a.js"
+  },
+  {
+    "revision": "0716e05899d4648497b1",
+    "url": "/Shoemaster/static/js/main.e39e08f2.chunk.js"
+  },
+  {
+    "revision": "ea9a8cbaab07105c4f8c",
+    "url": "/Shoemaster/static/js/2.4b17560e.chunk.js"
+  },
+  {
+    "revision": "0716e05899d4648497b1",
+    "url": "/Shoemaster/static/css/main.8e5dadbf.chunk.css"
+  },
+  {
+    "revision": "7dae0f46388ee628f385cc8517d9c3fc",
+    "url": "/Shoemaster/index.html"
+  }
+];
